da29419920e5d16869d054888c27ab4cd1dead7f
Ali Ghotbizadeh <ali.ghotbizadeh@gmail.com>
2023-12-05 19:36:12 +0330

    fix: change context to admin and create superuser

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,5 @@
 rs.status();
+use admin;
+const username = process.env['MONGO_INITDB_ROOT_USERNAME'];
+const password = process.env['MONGO_INITDB_ROOT_PASSWORD'];
+db.createUser({ user: username, pwd: password, roles: [{ role: 'root', db: 'admin' }] });
